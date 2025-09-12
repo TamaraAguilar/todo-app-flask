b05c7eee0aa41be6de2ebb0841f86748149a3e44
taguilar <taguilar@entel.cl>
2025-09-11 22:26:06 -0300

    add: add to do list

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap5(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(1000))
+    complete = db.Column(db.Boolean)
+
 @app.route('/')
 def index():
-    return "Hello World!"
+    #show all todos
+    todo_list = Todo.query.all()
+    return render_template('index.html', todo_list=todo_list)
+
+@app.route('/add', methods=['POST'])
+def add():
+    #add a new todo
+    title = request.form['title']
+    description = request.form['description']
+    complete= False # new tasks are incomplete by default
+    
+    new_todo = Todo(title=title, description=description, complete=complete)
+    
+    #add to the database
+    db.session.add(new_todo)
+    db.session.commit()
+    
+    #redirects to homepage
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Todo List Flask App
+
+[ENG] A simple todo list application built with Flask and SQLAlchemy.
+[SPA] Una app simple para crear una lista de tarea, construida usando Flask y SQLAlchemy
+
+## Setup
+
+1. Clone the repository
+2. Create virtual environment: `python -m venv .venv`
+3. Activate it: `.venv/Scripts/activate`
+4. Install dependencies: `pip install flask flask-sqlalchemy`
+5. Run: `python app.py`
